9926f586c110a5dbd5eefbd611325de28fdcdb04
RomanKovgan <r.kovgan88@gmail.com>
2022-07-12 15:44:51 +0300

    fixed __tests__ and __fixtures__

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,24 +1,16 @@
 import fs from 'fs';
-import buildAST from '../src/buildAST.js';
-import stylish from '../src/formatter/stylish.js';
-import plain from '../src/formatter/plain.js';
-import json from '../src/formatter/json.js';
-import { getPathFixture } from '../src/utils/utils.js';
-import dataParse from '../src/utils/parsers.js';
+import genDiff from '../src/index.js';
+import { getFixturePath } from '../src/utils/utils.js';
 
-const resultStylish = fs.readFileSync('__fixtures__/expectedStylish.txt', 'utf8');
-const resultPlain = fs.readFileSync('__fixtures__/expectedPlain.txt', 'utf8');
-const resultJson = fs.readFileSync('__fixtures__/expectedJson.txt', 'utf8');
+const formats = ['stylish', 'plain', 'json'];
+const types = ['json', 'yaml'];
 
-const data1 = dataParse(getPathFixture('file1.json'));
-const data2 = dataParse(getPathFixture('file2.json'));
-
-const unionTree = buildAST(data1, data2);
-
-test.each([
-  [stylish, resultStylish],
-  [plain, resultPlain],
-  [json, resultJson],
-])('%pformat of gendiff', (format, expected) => {
-  expect(format(unionTree)).toEqual(expected);
+describe.each(formats)('Comparison files', (format) => {
+  it.each(types)(`Testing format - ${format}`, (type) => {
+    const file1Path = getFixturePath(`file1.${type}`);
+    const file2Path = getFixturePath(`file2.${type}`);
+    const expectResult = fs.readFileSync(getFixturePath(`expected-${format}.txt`), 'utf8');
+    const result = genDiff(file1Path, file2Path, format);
+    expect(result).toBe(expectResult);
+  });
 });
